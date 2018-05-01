7176d759fd579d76aa6efa8a9100b21cdddc0b5d
cjacobme <cjacobme@aol.com>
2018-05-01 12:00:11 +0200

    refactored as a junit test

--- CJ  -  Spring  - Pro Spring 5 Parent/chapter03/injection/src/test/java/cj/software/spring/pro5/ch03/XmlConfigWithBeanFactoryTest.java
+++ CJ  -  Spring  - Pro Spring 5 Parent/chapter03/injection/src/test/java/cj/software/spring/pro5/ch03/XmlConfigWithBeanFactoryTest.java
@@ -1,12 +1,16 @@
 package cj.software.spring.pro5.ch03;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 
-public class XmlConfigWithBeanFactory
+public class XmlConfigWithBeanFactoryTest
 {
-	public static void main(String[] pArgs)
+	@Test
+	public void xmlBeanFactory()
 	{
 		DefaultListableBeanFactory lFactory = new DefaultListableBeanFactory();
 		XmlBeanDefinitionReader lBeanDefinitionReader = new XmlBeanDefinitionReader(lFactory);
@@ -14,6 +18,9 @@ public class XmlConfigWithBeanFactory
 				"spring/xml-bean-factory-config.xml");
 		lBeanDefinitionReader.loadBeanDefinitions(lClassPathResource);
 		Oracle lOracle = lFactory.getBean("oracle", Oracle.class);
-		System.out.println(lOracle.defineMeaningOfLife());
+		String lAnswer = lOracle.defineMeaningOfLife();
+		System.out.println(lAnswer);
+		assertThat(lAnswer).as("answer from Oracle").isEqualTo(
+				"Encyclopedias are a waste of time - go see the world instead");
 	}
 }
